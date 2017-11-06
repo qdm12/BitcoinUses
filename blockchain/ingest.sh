34d3a2031277ab0d9e5067c8c7dbf19cba11a55e
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-11-05 22:32:50 -0500

    Small update of shell script

--- blockchain/ingest.sh
+++ blockchain/ingest.sh
@@ -1,12 +1,13 @@
 sudo apt-add-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt-get install bitcoind
+sudo apt-get install -y bitcoind jq
 bitcoind -dbcache=4096 -daemon
-
-
+# This requires 4GB of ram, for faster sync
+# Wait several hours for the blockchain to be downloaded and verified
+# You can check the progress with:
 bitcoin-cli getblockchaininfo | jq -r '.verificationprogress'
-# hdfs dfs -mkdir -p /user/cloudera/bitcoin/input
-# hdfs dfs -put ~./.bitcoin/blocks/blk*.dat /user/cloudera/bitcoin/input
-hadoop fs -mkdir -p /user/spark/bitcoin/input
-hadoop fs -put ~./.bitcoin/blocks/blk*.dat /user/spark/bitcoin/input
-# rm -fr ~./.bitcoin
+
+# Once download and verification are complete
+hdfs dfs -mkdir -p /user/cloudera/bitcoin/input
+hdfs dfs -put ~./.bitcoin/blocks/blk*.dat /user/cloudera/bitcoin/input
+rm -fr ~./.bitcoin

7d85bfd6df888f0a5f343c29198301e88b7ebc18
acpg <acperezgea@gmail.com>
2017-12-08 17:03:25 -0500

    reddit data top ten words per month

--- analytic/impala.sql
+++ analytic/impala.sql
@@ -65,16 +65,24 @@ FROM blockchain;
 
 DROP TABLE blockchain;
 
-DROP TABLE IF EXISTS reddit;
-CREATE EXTERNAL TABLE reddit
+DROP TABLE IF EXISTS reddit_table;
+CREATE EXTERNAL TABLE reddit_table
 (
-   year INT,
-   month INT,
+   month STRING,
    word STRING,
+   score INT,
    count INT
 )
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
-LOCATION '/user/apg367/redditprofiler/output';
+LOCATION '/user/apg367/redditprofiler';
+
+DROP TABLE IF EXISTS reddit;
+CREATE TABLE reddit AS 
+-- https://community.hortonworks.com/questions/24667/hive-top-n-records-within-a-group.html
+SELECT * FROM (
+SELECT month, word, count, score, 
+rank() over ( partition by month ORDER BY count DESC) AS rank 
+FROM reddit_table ) t WHERE rank <= 10; --top ten words
 
 select 
   category, 

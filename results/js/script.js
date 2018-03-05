6642e533cea4d3709d63ecababc96a87bf4ac9f9
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-03-05 17:43:54 -0500

    Removed some useless animations

--- results/js/script.js
+++ results/js/script.js
@@ -24,66 +24,6 @@ function smoothScrolling() {
     });
 }
 
-function animateTitleWord(parent, wordIndex, period) {
-    // parent is title or subtitle
-    $("#introduction #" + parent + " #word" + wordIndex)
-    .animate({
-        "opacity": "1.00",
-    }, period / 2, function() {
-        $("#introduction #" + parent + " #word" + wordIndex).animate({
-            "opacity": "0.75",
-        }, period * 10);
-    });
-}
-
-function animateTitle(period) {
-    for (i = 1; i <= 3; i++) {
-        setTimeout(function(index, period){
-            animateTitleWord("title", index, period);
-        }, i*period, i, period);
-    }
-    for (i = 1; i <= 6; i++) {
-        setTimeout(function(index, period){
-            animateTitleWord("subtitle", index, period);
-        }, 3*period + i*0.5*period, i, period);
-    }
-}
-
-function animateTitleRepeat(period) {
-    animateTitle(period);
-    setInterval(function(){
-        animateTitle(period);
-    }, 11*period);
-}
-
-
-
-function bannerOpacity() {
-    var banner_opacity_mouseover = 0.93;
-    var banner_opacity_mouseout = 0.66;
-    var banner = $('#navigation_bar');
-    banner.mouseover(function(){
-        banner.css("opacity", banner_opacity_mouseover);
-    });
-    banner.mouseout(function(){
-        banner.css("opacity", banner_opacity_mouseout);
-    });
-    var b_pos = banner.offset().top;
-    $(window).scroll(function (){ // binds the following to when the user scrolls
-        var y = $(this).scrollTop();
-        if(y == 0){
-            banner_opacity_mouseover = 0.93;
-            banner_opacity_mouseout = 0.66;
-        } else {
-            banner_opacity_mouseover = 0.70;
-            banner_opacity_mouseout = 0.5;
-        }
-        banner.stop().animate({
-                        'opacity':banner_opacity_mouseout                
-                        },400);
-    });
-}
-
 window.onload = function() { /* executes first */
     resizeText();
     smoothScrolling();
@@ -95,8 +35,6 @@ window.onload = function() { /* executes first */
 };
 
 $(document).ready(function() { /* executes secondly */
-    bannerOpacity();
-    animateTitleRepeat(800);
     setTimeout(function(){getBlockchainResults();}, 400);
     setTimeout(function(){getRedditResults();}, 700);
     setTimeout(function(){getCoinmapResults();}, 1100);
